b8a1ad5579399cb31bbe2f1237c4b663226583ad
Tom Sullivan <tom@msbit.com.au>
2019-04-13 13:04:38 +1000

    Formatting

--- BirdMVVM/AppDelegate.swift
+++ BirdMVVM/AppDelegate.swift
@@ -1,18 +1,8 @@
-//
-//  AppDelegate.swift
-//  BirdMVVM
-//
-//  Created by tom on 13/4/19.
-//  Copyright © 2019 tom. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -40,7 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
--- BirdMVVM/ViewController/BirdViewController.swift
+++ BirdMVVM/ViewController/BirdViewController.swift
@@ -1,31 +1,20 @@
-//
-//  ViewController.swift
-//  BirdMVVM
-//
-//  Created by tom on 13/4/19.
-//  Copyright © 2019 tom. All rights reserved.
-//
-
 import UIKit
 
 class BirdViewController: UIViewController {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var purchaseFeeLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var purchaseFeeLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let image = UIImage(named: "Swifty")!
         let swifty = BirdModel(name: "Swifty", rarity: .veryRare, image: image)
-        
+
         let viewModel = BirdViewModel(model: swifty)
-        
+
         nameLabel.text = viewModel.name
         imageView.image = viewModel.image
         purchaseFeeLabel.text = viewModel.purchaseFeeText
     }
-
-
 }
-
