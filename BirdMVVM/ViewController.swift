e9673338d46ec799372e23db045d2f4951d3449b
Tom Sullivan <tom@msbit.com.au>
2019-04-13 12:13:54 +1000

    Initial Commit

---
+++ BirdMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirdMVVM
+//
+//  Created by tom on 13/4/19.
+//  Copyright © 2019 tom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
